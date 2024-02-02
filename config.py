ef7b0dd75f8776a8f304174215880b03fed8ddd7
pavel_pokidko <pp.market-sletat@yandex.ru>
2024-02-02 14:53:07 +0300

    add token

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 
 # BOT_TOKEN = '6970185567:AAE5Yj3RvdMKRrbyu9IKQHworiMKyhvI0vA' # токен https://t.me/assistant_market_sletat_bot
 
-BOT_TOKEN = '6792921931:AAHDUApX1xkPMspJ4O3XPLZ5HB7Od5_pDsk'  # токен https://t.me/test_marketsletat_local_bot
+# BOT_TOKEN = '6792921931:AAHDUApX1xkPMspJ4O3XPLZ5HB7Od5_pDsk'  # токен https://t.me/test_marketsletat_local_bot
 
 # BOT_TOKEN =  '6656906222:AAFXtqZrGcLXfIRitxt03JUbTXQYFLJsX6o' # токен https://t.me/test_marketsletat_bot
+
+BOT_TOKEN = '6863777524:AAFGp-2_CYcyWYUslWRImWd07YVFgurcqE4'  # токен https://t.me/marketsletat_review_bot
